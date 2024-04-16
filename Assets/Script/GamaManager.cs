1605b3904337c1792bd3edb0a311ab1265313fd0
05 <145641517+HongYeongHo@users.noreply.github.com>
2024-04-16 19:38:58 +0900

    add UI Function (2)
    add Mic, Manual, Option

---
+++ Assets/Script/GamaManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GamaManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+
+    [SerializeField] private float time;
+    [SerializeField] private float curTime;
+
+    int minute;
+    int second;
+
+    private void Awake()
+    {
+        StartCoroutine(StartTimer());
+    }
+
+    IEnumerator StartTimer()
+    {
+        curTime = time;
+        while (curTime > 0)
+        {
+            curTime += Time.deltaTime;
+            minute = (int)curTime / 60;
+            second = (int)curTime % 60;
+            text.text = minute.ToString("00") + ":" + second.ToString("00");
+            yield return null;
+
+            if (curTime >= 360)
+            {
+                Debug.Log("시간 종료");
+                curTime = 0;
+                yield break;
+            }
+        }
+    }
+}
